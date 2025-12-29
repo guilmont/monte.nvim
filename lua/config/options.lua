952db6b6ba1b2e8ae7fde94cd22572903f69fcf0
Gui Monteiro Oliveira <guigm.oliveira@gmail.com>
2025-12-29 11:12:10 +0100

    Add wildmenu and wildmode options for improved auto-completion

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -67,3 +67,7 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+-- Sane auto-completion options
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'longest:full,full'
