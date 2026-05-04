d046067c76433915a301a2a40dfa72705dc7ad20
Gui Monteiro Oliveira <guigm.oliveira@gmail.com>
2026-05-04 22:31:06 +0200

    Convert all tabs to spaces

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,13 @@
 -- Autocommands
 
+-- Force spaces for all filetypes, overriding ftplugins that set noexpandtab (e.g. Go)
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    vim.bo.expandtab = true
+  end,
+})
+
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
--- lua/plugins/guess-indent.lua
+++ lua/plugins/guess-indent.lua
@@ -3,6 +3,14 @@ return {
   'NMAC427/guess-indent.nvim',
   event = 'BufReadPost',
   config = function()
-    require('guess-indent').setup {}
+    require('guess-indent').setup {
+      override_editorconfig = true,
+      filetype_exclude = {},
+      buftype_exclude = { 'help', 'nofile', 'terminal', 'prompt' },
+      on_tab_options = {
+        expandtab = true,   -- keep spaces even when file uses tabs
+      },
+      on_space_options = {},
+    }
   end,
 }
